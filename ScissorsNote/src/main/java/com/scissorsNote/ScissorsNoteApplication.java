2e8bd876b45e46b0efd56f9a22aac8fcbe7df512
temlog <temlog.net@gmail.com>
2020-05-01 21:21:43 +0900

    commit

---
+++ ScissorsNote/src/main/java/com/scissorsNote/ScissorsNoteApplication.java
@@ -0,0 +1,13 @@
+package com.scissorsNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScissorsNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScissorsNoteApplication.class, args);
+	}
+
+}
---
+++ ScissorsNote/src/test/java/com/scissorsNote/ScissorsNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scissorsNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScissorsNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
