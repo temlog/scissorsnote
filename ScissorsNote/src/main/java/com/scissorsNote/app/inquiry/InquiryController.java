9968ca74dd8d4c39d6f98c95c6010cf5a1aa9640
temlog <temlog.net@gmail.com>
2020-05-05 14:42:01 +0900

    Spring入門34まで

--- ScissorsNote/src/main/java/com/scissorsNote/app/inquiry/InquiryController.java
+++ ScissorsNote/src/main/java/com/scissorsNote/app/inquiry/InquiryController.java
@@ -5,15 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
 	
 	@GetMapping("/form")
-	public String form(InquiryForm inquiryForm, Model model) {
+	public String form(InquiryForm inquiryForm,
+			Model model,
+			@ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
 	}
@@ -24,9 +28,9 @@ public class InquiryController {
 		return "inquiry/form";
 	}
 	
-	@PostMapping("/confirm")
+	@PostMapping("/confirm")//Formに入れた値が反映される
 	public String confirm(@Validated InquiryForm inquiryForm,
-			BindingResult result,
+			BindingResult result,//バリデーションが引っ掛かったらTrueが返る
 			Model model) {
 		if(result.hasErrors()) {
 			model.addAttribute("title", "Inqiry Form");
@@ -34,6 +38,19 @@ public class InquiryController {
 		}
 		model.addAttribute("title", "Confirm Page");
 		return "inquiry/confirm";
+	}
+	
+	@PostMapping("/complete")
+	public String complete(@Validated InquiryForm inquiryForm,
+			BindingResult result,
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/form";
+		}
+		redirectAttributes.addFlashAttribute("complete", "Registered!");
+		return "redirect:/inquiry/form";
 	}
 
 }
